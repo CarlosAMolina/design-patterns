302a91840221917cfdecd32d1ad5a89bfefebb64
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-05-11 07:29:07 +0200

    Update interpeter with the lexing part

--- behavioral-patterns/interpreter/handmade.py
+++ behavioral-patterns/interpreter/handmade.py
@@ -1,13 +1,13 @@
-from enum import Enum
+from enum import Enum, auto
 
 
 class Token:
     class Type(Enum):
-        INTEGER = 0
-        PLUS = 1
-        MINUS = 2
-        LPAREN = 3
-        RPAREN = 4
+        INTEGER = auto()
+        PLUS = auto()
+        MINUS = auto()
+        LPAREN = auto()
+        RPAREN = auto()
 
     def __init__(self, type, text):
         self.type = type
@@ -21,7 +21,7 @@ def lex(input):
     result = []
 
     i = 0
-    while i < len(input):
+    while i < len(input):  # Check each character.
         if input[i] == "+":
             result.append(Token(Token.Type.PLUS, "+"))
         elif input[i] == "-":
@@ -31,12 +31,12 @@ def lex(input):
         elif input[i] == ")":
             result.append(Token(Token.Type.RPAREN, ")"))
         else:  # must be a number
-            digits = [input[i]]
-            for j in range(i + 1, len(input)):
+            digits = [input[i]]  # first digit
+            for j in range(i + 1, len(input)):  # other digits
                 if input[j].isdigit():
                     digits.append(input[j])
-                    i += 1
-                else:
+                    i += 1  # Increment to avoid repeat again in the main while loop.
+                else:  # If no more digits, we have to managed all accumulated digits.
                     result.append(Token(Token.Type.INTEGER, "".join(digits)))
                     break
         i += 1
@@ -111,15 +111,16 @@ def parse(tokens):
 
 def eval(input):
     tokens = lex(input)
+    # Print each token to check they are correct.
     print(" ".join(map(str, tokens)))
 
-    parsed = parse(tokens)
-    print(f"{input} = {parsed.value}")
+    # parsed = parse(tokens)
+    # print(f"{input} = {parsed.value}")
 
 
 if __name__ == "__main__":
     eval("(13+4)-(12+1)")
-    eval("1+(3-4)")
+    # eval("1+(3-4)")
 
-    # this won't work
-    eval("1+2+(3-4)")
+    ## this won't work
+    # eval("1+2+(3-4)")
